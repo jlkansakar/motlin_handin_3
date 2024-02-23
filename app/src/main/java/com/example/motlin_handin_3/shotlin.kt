52e777e3022f600d5f0f62574d437ccc3ae152f0
jlkansakar <joka0002@stud.kea.dk>
2024-02-23 05:02:05 +0100

    finished assignment 2

--- app/src/main/java/com/example/motlin_handin_3/shotlin.kt
+++ app/src/main/java/com/example/motlin_handin_3/shotlin.kt
@@ -7,13 +7,64 @@ fun main() {
     val article4: Article = Article("John Doee", "Song of Ice and Fdire")
     val article5: Article = Article("John Doeb", "Song of Ice and Fighre")
     val articleList: List<Article> = arrayListOf(article1, article2, article3,article4, article5)
-    article1.toString()
+    println(articleList.toString())
 
+    val salatPizza: Pizza = Pizza("Salat Pizza", 1200, false, 8)
+    val pepsMedDress: Pizza = Pizza("Pepperoni Pizza", 1000, false, 8)
+    val phillyCheesesteak: Sandwich = Sandwich("Philly Cheesesteak", 700, false, 1)
+    val avocadoToast: Sandwich = Sandwich("Avocado Toast", 400, true, 1)
+    val foodList: List<FastFood> = arrayListOf(salatPizza, pepsMedDress, phillyCheesesteak, avocadoToast)
+    for (food in foodList) {
+        if (food is Pizza) {
+            food.addSauce("Dressing")
+        }
+        if (food is Sandwich) {
+            food.cutFood()
+        }
 
+    }
+    println(foodList.toString())
 }
 
 class Article(val author: String, val title: String){
     override fun toString(): String {
         return author
+    }
+}
+
+public interface FastFood {
+    val name: String
+    var calories: Int
+    val isItHealthy: Boolean
+    var numberOfFood: Int
+    fun cutFood(){
+        numberOfFood *= 2
+    }
+    fun addSauce(sauce: String){
+        calories = (calories * 1.5).toInt()
+    }
+
+}
+
+class Pizza(
+    override val name: String,
+    override var calories: Int,
+    override val isItHealthy: Boolean,
+    override var numberOfFood: Int
+
+): FastFood {
+    override fun toString(): String {
+        return "$name, $calories, $isItHealthy, $numberOfFood |"
+    }
+}
+
+class Sandwich(
+    override val name: String,
+    override var calories: Int,
+    override val isItHealthy: Boolean,
+    override var numberOfFood: Int
+): FastFood {
+    override fun toString(): String {
+        return "$name, $calories, $isItHealthy, $numberOfFood |"
     }
 }
